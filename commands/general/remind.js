326c42d982f7b6eb9fb5ac98de4acf61b41b1c81
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-17 01:09:46 -0500

    Update remind.js

--- commands/general/remind.js
+++ commands/general/remind.js
@@ -1,12 +1,16 @@
+const Discord = require('discord.js');
 const ms = require('ms');
     
     module.exports = {
         name: "remind",
-        category: "fun",
+        category: "Utilities",
         description: "Set's a reminder.",
-        run: async (client, message, args) => {
+        examples: "!remind [TIME][minutes, seconds, hours (m,s,h)]",
+        usage: "[time][M-S-H]",
+        cooldown: 5,
+        execute(bot, message, args) {
     
-            if (!client.lockit) client.lockit = [];
+            if (!bot.lockit) bot.lockit = [];
     
             let time = args[0];
             let validUnlocks = ['release', 'unlock'];
@@ -15,9 +19,9 @@ const ms = require('ms');
                 message.channel.overwritePermissions(message.author.id, {
                     SEND_MESSAGES: true,
                 }).then(() => {
-                    message.channel.send(`${message.author}**Your reminder is up!**`);
-                    clearTimeout(client.lockit[message.channel.id]);
-                    delete client.lockit[message.channel.id];
+                    message.channel.send(`${message.author}**Your reminder has been set!**`);
+                    clearTimeout(bot.lockit[message.channel.id]);
+                    delete bot.lockit[message.channel.id];
                 }).catch(error => {
                     console.log(error);
                 });
@@ -26,11 +30,11 @@ const ms = require('ms');
                     SEND_MESSAGES: true
                 }).then(() => {
                     message.channel.send(`${message.author}, I will remind you about ${args.slice(1).join(' ')} in ___${ms(ms(time), { long: true })}___.`).then(() => {    
-                        client.lockit[message.channel.id] = setTimeout(() => {
+                        bot.lockit[message.channel.id] = setTimeout(() => {
                             message.channel.overwritePermissions(message.author.id, {
                                 SEND_MESSAGES: true
                             }).then(message.channel.send(`${message.author}, you had asked me to remind you, here is your reminder. The title of this reminder is "${args.slice(1).join(' ')}."`)).catch(console.error);
-                            delete client.lockit[message.channel.id];
+                            delete bot.lockit[message.channel.id];
                         }, ms(time));
     
                     }).catch(error => {
