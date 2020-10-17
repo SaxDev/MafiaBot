84f90601f09497fd2dee479b90f2d5fca83823c5
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-17 00:03:05 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = async (client) => {
 
   //Client User activity
   client.user.setStatus("online");
-  client.user.setActivity("ModMail Thread!", { type: "WATCHING" });
+  client.user.setActivity("ModMail Threads!", { type: "WATCHING" });
 
   //Client info
   console.log('I have logged in and I am functioning.. I think');
