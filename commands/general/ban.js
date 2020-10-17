e1967215668a0c49c5ca7f558ea6d03a96f93e02
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-17 04:43:18 -0500

    Update ban.js

--- commands/general/ban.js
+++ commands/general/ban.js
@@ -21,14 +21,14 @@ module.exports = {
     if(member){
     member.kick(reason).then(() =>{
     message.channel.send({embed: {description:':tools: **${member.user.tag}** has been **KICKED**', color: '#42f12c'}}) 
-    member.send({embed: {description:`:x: | You were **kick** from **${message.guild}** for:` + reason, color: '#42f12c'}})
+    member.send({embed: {description:`You were **kick** from **${message.guild}** for:` + reason, color: '#42f12c'}})
     }).catch(err =>{
-    message.channel.send({embed: {description:`:x: | I was unable to kick ${user.tag}`, color: '#f83e42'}}).then(msg => msg.delete(3000));
+    message.channel.send({embed: {description:`I was unable to kick ${user.tag}`, color: '#f83e42'}}).then(msg => msg.delete(3000));
     console.log(err);
     })
     }
     } else {
-    message.channel.send({embed: {description:":x: | Please specify a user to kick!", color: '#f83e42'}}).then(msg => msg.delete(3000));
+    message.channel.send({embed: {description:"Please specify a user to kick!", color: '#f83e42'}}).then(msg => msg.delete(3000));
     }
      
   }
