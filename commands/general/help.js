2e7c256f21a34f35b3835b19f40b77a0039cb676
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-17 21:49:30 -0500

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,23 +1,17 @@
 const Discord = require("discord.js");
-const dev = require("../config.json");
 const fs = require('fs');
-
-function prettyString(string) {
- return string.replace(/_/g, " ").replace(/guild/gi, "Server").replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();})
-}
 
 module.exports = {
   name: "help",
   category: "General",
   description: "Shows the list of commands.",
   aliases: ["commands"],
-  examples: "gn!help embed\ngn!commands",
   usage: "[command name]",
   cooldown: 5,
   execute(bot, message, args) {
     const categories = fs.readdirSync('./commands').filter(file => !file.endsWith('.js'));
     const misc = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-    const dev = fs.readdirSync('./commands/dev').filter(file => file.endsWith('.js'));
+
     if (!args[0]) {
 
       let helpEmbed = new Discord.MessageEmbed()
@@ -26,8 +20,10 @@ module.exports = {
       .setTimestamp()
       .setFooter("Requested by " + message.author.username, message.author.avatarURL())
       .setColor("BLUE");
-     
-      categories.forEach(category => {     
+
+      categories.forEach(category => {
+        if (category === "dev") return;
+
         let helpList = [];
         const commands = fs.readdirSync(`./commands/${category}`).filter(file => file.endsWith('.js'));
 
@@ -40,6 +36,7 @@ module.exports = {
 
         helpEmbed.addField(categoryName, helpList.join(", "));
       });
+
       if (misc.length < 0) {
         let miscList = []
         misc.forEach(c => {
@@ -52,24 +49,19 @@ module.exports = {
     } else {
 
       let command = bot.commands.get(args[0].toLowerCase())
-      if (!command) return message.channel.send({embed: {title: "Error ⚠️", description:`There's no such a command like that, to see the full command list please use \`${message.prefix}commands\` command.`, color: '#f83e42'}});
+      if (!command) return message.channel.send(`<:cross:724049024943915209> | There's no such a command like that, to see the full command list please use \`${message.prefix}commands\` command. `);
 
-      let perms = []
-      if (command.reqPermissions) {
-        command.reqPermissions.forEach(perm => {
-          perms.push(prettyString(perm))
-        })
-      }
       let helpEmbed = new Discord.MessageEmbed()
       .setTitle(command.name)
-      .setFooter("The usage in <> is required, [] is optional. Requested by " + message.author.tag, message.author.avatarURL())
-      .setColor("#fffff0")
+      .setDescription('The usage in <> is required, [] is optional.')
+      .setFooter("Requested by " + message.author.tag, message.author.avatarURL())
+      .setColor("#ffffff")
       if (command.description) helpEmbed.addField("**Description**", command.description);
-      if (command.category) helpEmbed.addField("**Category**", command.category, true)
-      if (command.aliases) helpEmbed.addField("**Aliases**", command.aliases.join(', '), true);
-      if (command.usage) helpEmbed.addField("**Usage**", `\`${message.prefix + command.name} ${command.usage}\``, true);
-      if (command.examples) helpEmbed.addField("**Examples**", `\`\`\`${command.examples}\`\`\``);
-      if (command.reqPermissions) helpEmbed.addField("**Required Permission(s)**", perms);
+      if (command.category) helpEmbed.addField("**Category**", command.category)
+      if (command.aliases) helpEmbed.addField("**Aliases**", command.aliases.join(', '));
+      if (command.usage) helpEmbed.addField("**Usage**", message.prefix + command.name + " " + command.usage);
+      if (command.examples) helpEmbed.addField("**Examples**", command.examples);
+      if (command.reqPermissions) helpEmbed.addField("**Required Permission(s)**", command.reqPermissions.join(', '));
       if (command.guildOnly) helpEmbed.addField("**Guild Only**", "Command only can be executed in a server.");
       if (command.dmOnly) helpEmbed.addField("**DM Only**", "Command only can be executed in DMs.");
       if (command.dev) helpEmbed.addField("**Dev**", "Only Bot Developers can execute this command.");
