9338e7ea453c7ea7410c5644e9adbdbb869745f3
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-18 00:27:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 //Discord
 const { Client, Collection } = require('discord.js');
+const db = require("mongoose")
 const client = new Client({
-  disableMentions: "everyone"
 });
-
+const dbOptions = {
+   userNewUrlParser: true,
+   useUnifiedTopology: true,
+   useFindAndModify: true
+}
 client.commands = new Collection();
 client.events = new Collection();
 client.ready = false;
@@ -13,4 +17,6 @@ client.ready = false;
   require(`./handlers/${handler}`)(client);
 });
 
+    await db.connect("mongodb://Pm277353:Pm277353@cluster0-shard-00-00.fwqk7.mongodb.net:27017,cluster0-shard-00-01.fwqk7.mongodb.net:27017,cluster0-shard-00-02.fwqk7.mongodb.net:27017/ModMail-Assistant?ssl=true&replicaSet=atlas-5hhyyt-shard-0&authSource=admin&retryWrites=true&w=majority"
+          .then(console.log("Database active"))
 client.login(process.env.BOT_TOKEN);
