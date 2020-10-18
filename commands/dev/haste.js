ebeb0c1675af5eae0ddc9ca30bb08b8eb3fafe65
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-17 22:09:38 -0500

    Update haste.js

--- commands/dev/haste.js
+++ commands/dev/haste.js
@@ -4,7 +4,7 @@ const hastebin = require("hastebin-gen");
 
 module.exports = {
   name: "haste-bin",
-  category: "dev",
+  category: "Dev",
   description: "Uploads stuff to Hastebin. DEV ONLY.",
   aliases: ["up","haste"],
   usage: "[text]",
