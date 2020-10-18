eda76d88377e99dbb3836ea9c0953bbef7d73d69
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-18 00:33:54 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,4 @@
+const db = require("mongoose")
 module.exports = async (client) => {
 
   //Client User activity
@@ -8,4 +9,12 @@ module.exports = async (client) => {
   console.log('I have logged in and I am functioning.. I think');
 
   client.ready = true;
+  
+  const dbOptions = {
+   userNewUrlParser: true,
+   useUnifiedTopology: true,
+   useFindAndModify: true
+}
+   db.connect("mongodb://Pm277353:Pm277353@cluster0-shard-00-00.fwqk7.mongodb.net:27017,cluster0-shard-00-01.fwqk7.mongodb.net:27017,cluster0-shard-00-02.fwqk7.mongodb.net:27017/ModMail-Assistant?ssl=true&replicaSet=atlas-5hhyyt-shard-0&authSource=admin&retryWrites=true&w=majority"
+          console.log("Database active");
 };
