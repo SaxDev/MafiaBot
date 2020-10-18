9ab1473f985f647ee65c2cacbc90464abd83ef2a
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-17 22:47:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,5 @@ client.ready = false;
 ["commands", "events"].forEach(handler => {
   require(`./handlers/${handler}`)(client);
 });
- let ignorechannels = ["740424191798673428","740412152728256588","762712565444509736","740418976852738159","740417504601571349","740429722885947455","740429803856855052"]
 
-    if (!message.channel.id = ignorechannels.forEach(channelid =>)) return;{
-    function oneWorking() {
-    setTimeout(() => {
-        message.channel.send('yeet?');
-    }, 30000);
-}
-  })
 client.login(process.env.BOT_TOKEN);
