7faf807320f3829e001c3cebaa957f7fccead47f
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-17 21:05:25 -0500

    Update purge.js

--- commands/general/purge.js
+++ commands/general/purge.js
@@ -9,12 +9,12 @@ module.exports = {
   guildOnly: true,
   reqPermissions: ['MANAGE_MESSAGES'],
   execute(bot, message, args) {
-      if (!message.guild.me.hasPermission(["MANAGE_MESSAGES"])) return message.channel.send({embed: {description:"I do not have the required permissions!", color: '#f83e42'}}).then(msg => msg.delete(5000))
+      if (!message.guild.me.hasPermission(["MANAGE_MESSAGES"])) return message.channel.send({embed: {title: "Error ⚠️", description:"I do not have the required permissions!", color: '#f83e42'}}).then(msg => msg.delete(5000))
   
     if (!message.member.hasPermission("MANAGE_MESSAGES"))
-    return message.channel.send({embed: {description:'You do not have the required permissions!', color: '#f83e42'}}).then(msg => msg.delete(5000))
+    return message.channel.send({embed: {title: "Error ⚠️", description:'You do not have the required permissions!', color: '#f83e42'}}).then(msg => msg.delete(5000))
   
-    if (isNaN(args[0]) || parseInt(args[0] <= 0)) return message.channel.send({embed: {description:'Please provide a amount! No letters.', color: '#f83e42'}}).then(msg => msg.delete(3000))
+    if (isNaN(args[0]) || parseInt(args[0] <= 0)) return message.channel.send({embed: {title: "Error ⚠️", description:'Please provide a amount! No letters.', color: '#f83e42'}}).then(msg => msg.delete(3000))
      
     let deleteAmmount;
 
@@ -25,6 +25,6 @@ module.exports = {
     }
 
     message.channel.bulkDelete(deleteAmmount, true)
-    .then(deleted => message.channel.send({embed: {description: `${message.author}, I have deleted \`${deleted.size}\` messages.`, color: '#42f12c'}})).then(msg => msg.delete(5000))
+    .then(deleted => message.channel.send({embed: {title: "Success!", description: `${message.author}, I have deleted \`${deleted.size}\` messages.`, color: '#42f12c'}})).then(msg => msg.delete(5000))
     }
 };
