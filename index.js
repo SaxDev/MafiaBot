372c3a0f37d62d18926b8b37cc0f98b93324a673
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-19 04:43:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 //Discord
+const mongoose = require('mongoose');
 const { Client, Collection } = require('discord.js');
 const client = new Client({
 });
+
+mongoose.connect('mongodb+srv://Pm277353:Pm277353@cluster0.fwqk7.mongodb.net/test/Data', { newNewUrlParser: true, useUnifiedTopology: true} )
 
 client.commands = new Collection();
 client.events = new Collection();
