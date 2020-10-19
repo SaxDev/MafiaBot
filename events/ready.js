5d4a5fb396183f5d9476411b9e79ba673734eca2
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-19 03:41:29 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,17 @@
 module.exports = async (client) => {
-
   //Client User activity
   client.user.setStatus("online");
   client.user.setActivity("ModMail Threads!", { type: "WATCHING" });
 
   //Client info
-  console.log('I have logged in and I am functioning.. I think');
+const response1 = 'ModMail Assistant is online, listening to port 42069'
+const response2 = 'ight daddy, I am ready :smirk_cat:'
+const response = [
+  response1,
+  response2
+                 ]
+  var pick = Math.floor(Math.random() * response.length);
+   console.log(response[pick]);
 
   client.ready = true;
 
