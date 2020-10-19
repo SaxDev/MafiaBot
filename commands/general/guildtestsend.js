4368c990d533f7a3b984969fcd7b2e4875419450
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-19 01:53:47 -0500

    Create guildtestsend.js

---
+++ commands/general/guildtestsend.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "sendembed",
+  category: "General",
+  description: "Sends a message with a fancy embed.",
+  usage: "[channel] | [title] | [description] | [color]",
+  cooldown: 5,
+  guildOnly: true,
+  reqPermissions: ['MANAGE_GUILD'],
+  execute(bot, message, args) {
+  
+        var guildList = client.guilds.array();
+        try {
+            guildList.forEach(guild => guild.defaultChannel.send("pp");
+        } catch (err) {
+            console.log("Could not send message to " + guild.name);
+        
+    }
+  }
+};
