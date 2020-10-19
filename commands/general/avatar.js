3e3fdc83c6ec8bf57babe74f35c6090a694a22ad
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-19 01:16:18 -0500

    Create avatar.js

---
+++ commands/general/avatar.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "avatar",
+  category: "General",
+  description: "Get a specified user's avatar.",
+  aliases: ["av"],
+  usage: "[user]",
+  cooldown: 5,
+  async execute(bot, message, args) {
+    let user
+    if (!args[0]) {
+      user = message.author
+    } else {
+      user = message.mentions.users.first()
+      if (!user) return message.channel.send({embed: {title: "Error ⚠️", description:"You didn't provide a true user.", color: '#f83e42'}})
+    }
+
+    message.channel.send({
+      embed: {
+        description: `Here is **${user.tag}**'s avatar:`,
+        image: {
+          url: user.avatarURL({format:"png",dynamic:true,size:1024})
+        },
+        color: "RANDOM",
+        footer: {
+          text: `Requested by: ${message.author.tag}`,
+          icon: message.author.avatarURL({dynamic:true})
+        }
+      }
+    })
+  }
+};
