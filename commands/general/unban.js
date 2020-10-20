86f75191eacf00b22af194f4300e13715f1e5e4b
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-20 07:16:14 -0500

    Update unban.js

--- commands/general/unban.js
+++ commands/general/unban.js
@@ -17,14 +17,10 @@ module.exports = {
     if (bans.some(u => userID.includes(u.username)));
     let bUser = bans.find(user => user.username === userID);
     let reason = args.slice(1).join(' ');
-    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || bans.find(b => b.user.id == userID)    
-    if(!member)
-        return message.channel.send({embed: {title: "Error⚠️", description:"Please mention a valid member of this server", color:'#f83e42'}});
-    
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || bans.find(b => b.user.id == userID)        
     if(!reason) reason = "No reason provided.";
     
-    if(!bUser) return message.channel.send({embed: {title: "Error⚠️", description: "Please provide a user to unban.", color: '#f5ce42'}});
-    if(!member) return message.channel.send({embed: {title: "Error⚠️", description: "Please provide a user to unban.", color: '#f5ce42'}});                
+    if(!bUser) return message.channel.send({embed: {title: "Error⚠️", description: "Please provide a user to unban.", color: '#f5ce42'}});             
       message.guild.members.unban(bUser.user({reason: reason}));
       message.guild.members.unban(bUser.user.id({reason: reason}));
       message.channel.send({embed: {title: "Success!", description: `I have successfully unbanned ${member.users.tag} for: ${reason}`, color: '#42f12c'}});  
