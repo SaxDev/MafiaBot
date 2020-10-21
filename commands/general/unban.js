df9416117f21234731138c16d1f00711b6424ae9
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-21 05:21:23 -0500

    Update unban.js

--- commands/general/unban.js
+++ commands/general/unban.js
@@ -3,26 +3,21 @@ const Discord = require("discord.js");
 module.exports = {
   name: "unban",
   category: "General",
-  description: "Ban a user from the server!",
-  usage: "[user] <reason>",
+  description: "Unban a user from the server!",
+  usage: "[user]",
   aliases: ["un-ban","pardon","unbean","un-bean"],
   cooldown: 5,
   guildOnly: true,
   reqPermissions: ['BAN_MEMBERS'],
   execute(bot, message, args) {
-    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided.";
+
       let userID = args[0]
       message.guild.fetchBans().then(bans=> {
       if(bans.size == 0) return message.channel.send({embed: {title: "Error⚠️", description: "No users are banned in this guild!", color: '#f5ce42'}});      
       let bUser = bans.find(b => b.user.id == userID)
       if(!bUser) return message.channel.send({embed: {title: "Error⚠️", description: "Please provide a user to unban.", color: '#f5ce42'}});
-      message.guild.members.unban(bUser.user({reason: reason}))
-            .catch(error => message.channel.send({embed: {title: "Error⚠️", description: 'I could not seem to unban that user.', color:'#f83e42'}}));
-      message.channel.send({embed: {title: "Success!", description: `I have successfully unbanned ${member.users.tag} for: ${reason}`, color: '#42f12c'}});
-      if(member.bUser)
-         return message.channel.send({embed: {title: "Success!", description:`A user has been un-banned by ${message.author.tag} for: ${reason}`, color:'#42f12c'}});
+      message.guild.members.unban(bUser.user)
+      message.channel.send({embed: {title: "Success!", description: 'I have successfully unbanned' + bUser.user, color: '#42f12c'}});
 })
   }
 };
