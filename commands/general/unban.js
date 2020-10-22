58f47cc2f93b65c7c9e1965f2a3af7e10946c819
noodles69 <66779548+noodles69@users.noreply.github.com>
2020-10-22 00:53:34 -0500

    Update unban.js

--- commands/general/unban.js
+++ commands/general/unban.js
@@ -11,13 +11,14 @@ module.exports = {
   reqPermissions: ['BAN_MEMBERS'],
   execute(bot, message, args) {
     let userID = args[0]
-    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    let unbanned = message.mentions.users.first() || bot.users.resolve(args[0]);
+    let member = bot.users.fetch(unbanned);
     let reason = args.slice(1).join(' ' + ` - Responsible Mod: ${message.author.tag}`);
        message.guild.fetchBans().then(bans=> {
       if(bans.size == 0) return 
     let bUser = bans.find(b => b.user.id == userID)
       if(!bUser) return
-       message.channel.send({embed: {title: "Success!", description:`${member.user.tag} has been un-banned by ${message.author.tag} for: ${reason}`, color:'#42f12c'}});
+       message.channel.send({embed: {title: "Success!", description:`${member.tag} has been un-banned by ${message.author.tag} for: ${reason}`, color:'#42f12c'}});
        message.guild.members.unban(bUser.user)
 })
   }
